976a572f6acebf47ebc54e7c777f1629e08e6903
Pooja Ranawade <poojaranawade@users.noreply.github.com>
2018-11-08 13:17:09 -0800

    Phase 1: All files

---
+++ data.sql
@@ -0,0 +1,15 @@
+INSERT INTO `wallet`.`user` (`active`, `balance`, `email`, `firstname`, `lastname`, `password`)
+	VALUES (1, '1000',  'email1',  'fname1',  'lname1',  'pass1');
+INSERT INTO `wallet`.`user` (`active`, `balance`, `email`, `firstname`, `lastname`, `password`)
+	VALUES (1,  '2000',  'email2',  'fname2',  'lname2',  'pass2');
+INSERT INTO `wallet`.`user` (`active`, `balance`, `email`, `firstname`, `lastname`, `password`)
+	VALUES (1,  '3000',  'email3',  'fname3',  'lname3',  'pass3');
+INSERT INTO `wallet`.`user` (`active`, `balance`, `email`, `firstname`, `lastname`, `password`)
+	VALUES (1,  '4000',  'email4',  'fname4',  'lname4',  'pass4');
+INSERT INTO `wallet`.`user` (`active`, `balance`, `email`, `firstname`, `lastname`, `password`)
+	VALUES (1,  '5000',  'email5',  'fname5',  'lname5',  'pass5');
+INSERT INTO `wallet`.`user` (`active`, `balance`, `email`, `firstname`, `lastname`, `password`)
+	VALUES (1,  '6000',  'email6',  'fname6',  'lname6',  'pass6');
+INSERT INTO `wallet`.`user` (`active`, `balance`, `email`, `firstname`, `lastname`, `password`)
+	VALUES (1,  '7000',  'email7',  'fname7',  'lname7',  'pass7');
+
